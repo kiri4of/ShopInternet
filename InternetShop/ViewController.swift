3d9049a6fc4d805902eca6a45dd109461c70c539
kiri4of <kopak7890@gmail.com>
2022-04-20 16:04:15 +0300

    add gitignore and Code files

---
+++ InternetShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternetShop
+//
+//  Created by Kiri4of on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+        // NEW thing
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
